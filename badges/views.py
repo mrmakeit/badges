cd4376c96d7bd3cf85d7c7471d599292f5e7e97d
Blixa Morgan <blixa@projectmakeit.com>
2014-02-23 17:58:47 +0000

    add user page

--- badges/views.py
+++ badges/views.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.template import RequestContext
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as auth_logout
+from badger.models import Award
+from django.contrib.auth.models import User
 
 def logout(request):
     """Logs out user"""
@@ -17,6 +19,15 @@ def home(request):
     return render_to_response('home.html', {
     }, RequestContext(request))
 
+@login_required
+def user(request):
+    """user view, displays user information"""
+    user = get_object_or_404(User, username=request.user)
+    awards = Award.objects.filter(user=user)
+    return render_to_response('user.html',{ 
+		'user': user,
+		'award_list': awards,
+    }, RequestContext(request))
 
 @login_required
 def badges(request):
